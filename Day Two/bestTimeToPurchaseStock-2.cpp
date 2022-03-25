9bc7f3dbfd17e46e3ffb139831953ae3b8ff6305
Pratham Vasani <73855805+JoKeR-VIKING@users.noreply.github.com>
2022-03-25 16:44:39 +0530

    Create bestTimeToPurchaseStock-2.cpp

---
+++ Day Two/bestTimeToPurchaseStock-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int profit = 0, minPrice = prices[0];
+        
+        for (int i=1;i<prices.size();i++)
+        {
+            if (prices[i] > minPrice)
+                profit += prices[i] - minPrice;
+            
+            minPrice = prices[i];
+        }
+        
+        return profit;
+    }
+};
