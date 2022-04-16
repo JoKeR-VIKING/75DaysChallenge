a745f144fb043f00543a591b9f975419fe9ac628
Pratham Vasani <73855805+JoKeR-VIKING@users.noreply.github.com>
2022-04-16 10:42:23 +0530

    Create stackUsingQueue.cpp

---
+++ Day Twenty Four/stackUsingQueue.cpp
@@ -0,0 +1,69 @@
+class MyStack 
+{
+private:
+    queue<int> q1;
+public:
+    MyStack() {}
+    
+    void push(int x) 
+    {
+        q1.push(x);
+    }
+    
+    int pop() 
+    {
+        queue<int> q2;
+        while (q1.size() > 1)
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        
+        int ans = q1.front();
+        q1.pop();
+        
+        while (!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+        
+        return ans;
+    }
+    
+    int top() 
+    {
+        queue<int> q2;
+        while (q1.size() > 1)
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        
+        int ans = q1.front();
+        q2.push(ans);
+        q1.pop();
+        
+        while (!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+        
+        return ans;
+    }
+    
+    bool empty() 
+    {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
