3805ec0b30ba17eaad22c79bd723601227bad601
Pratham Vasani <73855805+JoKeR-VIKING@users.noreply.github.com>
2022-04-23 14:55:12 +0530

    Create pathSumIII.cpp

---
+++ Day Thirty Two/pathSumIII.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void backTrack(TreeNode* root, int targetSum, int& res)
+    {
+        if (root == NULL)
+            return;
+        if (root -> val == targetSum)
+            res++;
+        
+        backTrack(root -> left, targetSum - root -> val, res);        
+        backTrack(root -> right, targetSum - root -> val, res);
+    }
+    
+    void getPathSum(TreeNode* root, int targetSum, int& res)
+    {
+        if (root == NULL)
+            return;
+        
+        backTrack(root, targetSum, res);
+        getPathSum(root -> left, targetSum, res);        
+        getPathSum(root -> right, targetSum, res);
+    }
+    
+    int pathSum(TreeNode* root, int targetSum) 
+    {
+        int res = 0;
+        getPathSum(root, targetSum, res);
+        
+        return res;
+    }
+};
