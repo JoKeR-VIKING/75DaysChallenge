b0a8ad0a9d6c5d6c44add36af407247b8bb755af
Pratham Vasani <73855805+JoKeR-VIKING@users.noreply.github.com>
2022-05-18 00:23:07 +0530

    Create shortestPath.cpp

---
+++ Day Fifty Four/shortestPath.cpp
@@ -0,0 +1,13 @@
+class Solution{   
+public:
+    int minimumStep(int n)
+    {
+        if (n == 1)
+            return 0;
+        
+        if (n % 3 == 0)
+            return 1 + minimumStep(n / 3);
+        
+        return 1 + minimumStep(n - 1);
+    }
+};
